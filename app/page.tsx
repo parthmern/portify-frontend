188d7c7cbfaf2dabe338e187749552529b95d503
parthmern <parthmern@gmail.com>
2024-12-18 23:21:41 -0500

    video dialog added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Navbar  from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 import dynamic from "next/dynamic";
 import Particles from "@/app/components/particles";
@@ -9,41 +9,58 @@ import AnimatedShinyText from "./components/animated-shiny-text";
 import { AnimatedClickBtn } from "./components/AnimatedClickBtn";
 import WordPullUp from "./components/word-pull-up";
 import BlurIn from "./components/blur-in";
-const ThemeComponent = dynamic(()=> import("./components/theme-switch"),{ssr:false});
-
+import { RainbowButton } from "./components/rainbow-button";
+import HeroVideoDialog from "./components/hero-video-dialog";
+const ThemeComponent = dynamic(() => import("./components/theme-switch"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div className="dark:text-black overflow-x-hidden  dark:bg-white text-white bg-[#08090a] h-screen">
-      
-       
-        {/* <ThemeComponent /> */}
-        <Navbar />
-        <div className="relative">
+      {/* <ThemeComponent /> */}
+      <Navbar />
+      <div className="relative">
         <div className="z-[100] w-full absolute">
-            <div className="mx-auto">
-              <AnimatedClickBtn />
-            </div>
-            <div className="mx-auto flex flex-col items-center justify-center">
-              <p className="text-5xl font-medium">Launch your AI powered </p>
-              <p className="text-5xl mt-4 font-medium">Portfolio <BlurIn className="noricanFont text-[#3ECF8E] neon" word="in minutes" /> </p>
-              
-            </div>
-            
+          <div className="mx-auto">
+            <AnimatedClickBtn />
+          </div>
+          <div className="mx-auto -mt-10 flex flex-col items-center justify-center">
+            <p className="text-5xl font-medium">Launch your AI powered </p>
+            <p className="text-5xl mt-4 font-medium">
+              Portfolio{" "}
+              <BlurIn
+                className="noricanFont text-[#3ECF8E] neon"
+                word="in minutes"
+              />{" "}
+            </p>
+          </div>
+          <div className="mx-auto flex flex-col mt-14 items-center justify-center">
+            <RainbowButton className="" children="Try Now" />
+          </div>
         </div>
+      </div>
+
+      <Particles />
+
+      <div className="w-full h-full mt-11 ">
+        <div className="relative z-[9999] glowing-box w-[70%] mx-auto">
+          <HeroVideoDialog
+            className="dark:hidden block"
+            animationStyle="from-center"
+            videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+            thumbnailSrc="https://res.cloudinary.com/dncm3mid4/image/upload/v1734582023/githubreadme/wo78srzkxifjhqsm34nu.jpg"
+            thumbnailAlt="Hero Video"
+          />
+          <HeroVideoDialog
+            className="hidden dark:block"
+            animationStyle="from-center"
+            videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+            thumbnailSrc="https://res.cloudinary.com/dncm3mid4/image/upload/v1734581885/githubreadme/l4jwwoel56d9hduovjxh.jpg"
+            thumbnailAlt="Hero Video"
+          />
         </div>
-
-        <Particles />
-
-        <div className="absolute ">
-        
-        </div>
-
-        
-        
-      
+      </div>
     </div>
   );
 }
-
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,7 +49,12 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
-  			}
+  			},
+  			'color-1': 'hsl(var(--color-1))',
+  			'color-2': 'hsl(var(--color-2))',
+  			'color-3': 'hsl(var(--color-3))',
+  			'color-4': 'hsl(var(--color-4))',
+  			'color-5': 'hsl(var(--color-5))'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
@@ -57,7 +62,8 @@ export default {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		animation: {
-  			'shiny-text': 'shiny-text 8s infinite'
+  			'shiny-text': 'shiny-text 8s infinite',
+  			rainbow: 'rainbow var(--speed, 2s) infinite linear'
   		},
   		keyframes: {
   			'shiny-text': {
@@ -66,6 +72,14 @@ export default {
   				},
   				'30%, 60%': {
   					'background-position': 'calc(100% + var(--shiny-width)) 0'
+  				}
+  			},
+  			rainbow: {
+  				'0%': {
+  					'background-position': '0%'
+  				},
+  				'100%': {
+  					'background-position': '200%'
   				}
   			}
   		}
